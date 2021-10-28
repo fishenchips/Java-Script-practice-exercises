8730520c40e2c3f104a8baea3719035635dc6de7
Philip Carlsson <philipcarlsson@hotmail.com>
2021-10-28 15:17:24 +0200

    new solutions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Skapa ett program som kan beräkna summan av två tal 
+//Övning 1 Skapa ett program som kan beräkna summan av två tal 
 
 function addition (a, b) {
     console.log(a+b)
@@ -7,7 +7,7 @@ function addition (a, b) {
 addition (10, 20)
 
 
-// Skapa ett program som kan räkna udda och jämna tal från listan och skriva svaren i konsolen. 
+// Övning 2 Skapa ett program som kan räkna udda och jämna tal från listan och skriva svaren i konsolen. 
 
 var lista = [1,2,3,4,5,6,7,8,9,10]
 
@@ -20,8 +20,10 @@ else {
 }
 }
 
+// Bör även addera lista[i] i console log så man ser själva svaret, just nu har jag bara string value.
 
-// Skapa en for loop som kan loopa igenom listan ovan och skriva ut varje enskilt värde 
+
+//Övning 3 Skapa en for loop som kan loopa igenom listan ovan och skriva ut varje enskilt värde 
 
 lista.map (function(e) {
     console.log(e)
@@ -29,6 +31,9 @@ lista.map (function(e) {
 
 console.log(lista)
 
+//lista.map( (e) =>console.log(e) ) ARROW FUNCTION
+
 
 // Skapa en funktion som tar 2 tal som argument och returnera summan av två talen
 // 4.1) skriva ut summan i en div i html 
+
