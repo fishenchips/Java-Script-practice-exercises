d763c07ae50404b6d16d0bf6570c50f04e3eb037
Philip Carlsson <philipcarlsson@hotmail.com>
2021-10-30 15:25:08 +0200

    more answers

--- main.js
+++ main.js
@@ -52,3 +52,45 @@ document.querySelector (".addition").textContent = calculate (50, 51)
 
 // Övning 5 Skapa en funktion expression som  tar en string som argument och skriver ut i consolen.
 //  * funktionen också returnera strängen. Spara trängen i en variabel. 
+
+// Övning 6 Skapa en funktion som kan konvertera en string till en array 
+// till ex. var x = Rakib ska bli x= [R,a,k,i,b] 
+
+var x = "Rakib";
+
+console.log(x.split(""))
+
+//console.log(x.split("").reverse().join(""))
+
+
+//Övning 7 Skapa en Shopping List app med hjälp av DOM manipulering.  Användare kan lägga till shopping list 
+// och ta bort  items  från shopping list. tips: appendChild(), remove()
+
+
+
+
+
+function addToShopping(e) {
+    e.preventDefault();
+    const input = document.querySelector("#add-shopping").value
+    console.log(input)
+    const newLi = document.createElement ("li")
+    newLi.textContent = input
+    const shoppingList = document.querySelector(".shopping-div")
+    shoppingList.appendChild(newLi)
+} 
+
+
+//Trying to remove shopping list --- NOT WORKING
+function removeShopping (){ 
+    var div = document.getElementsByClassName("shopping-div");
+    div(remove);
+}
+
+// nedan behöver vara utanför? funkar inte om den är i funktionen (att console logga)
+document.querySelector("#btn-shopping").addEventListener("click", addToShopping)
+
+document.querySelector("#remove").addEventListener("click", removeShopping)
+
+
+// Övning 8 Skapa en funktion som tar string som argument och returnera reverse/omvänd värde
